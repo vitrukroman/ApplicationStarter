2f4254651f24ab1725e3325f38c86a98e8478865
Roman Vitruk <vitrukrom@gmail.com>
2018-03-20 20:14:27 +0200

    rm

--- OfflineKaraokeAPI/src/routes/routes.ts
+++
@@ -1,8 +0,0 @@
-import {Application} from "express";
-import authRoutes from "./authRoutes";
-import usersRoutes from "./usersRoutes";
-
-export default (app: Application) => {
-  authRoutes(app);
-  usersRoutes(app);
-};
--- OfflineKaraokeAPI/src/services/services.ts
+++
@@ -1,6 +0,0 @@
-import {IConfig} from "../../config/createConfig";
-import googleAuth from "./googleAuth";
-
-export default (config: IConfig) => {
-  googleAuth(config);
-};
